35ef424f79e5ebb3af921faa65bd4297ff987f40
Vuong Khuat <vuongkhuat0407@gmail.com>
2017-07-06 14:14:59 -0400

    update analogy_strings.py with more patterns

--- analogy_strings.py
+++ analogy_strings.py
@@ -1,1 +1,4 @@
-analogy_string_list = [['is', 'like', 'a'], ['analogous'], ['analogy']]
+analogy_string_list = [['is', 'like'], ['are', 'like'], ['like', 'a'], ['like', 'an'], ['analogous'],
+					['analogy'], ['just', 'as'], ['similar', 'to'], ['similarly'], ['the', 'same', 'as'],
+					['likewise'], ['equally'], ['equivalent', 'to'], ['identical', 'to'],
+					['comparable', 'to']]
