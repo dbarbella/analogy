4a96db5b2d52fb6a923495e9989ae20f4f479477
Vuong Khuat <vuongkhuat0407@gmail.com>
2017-07-10 11:36:37 -0400

    update wordhunt, analogy_strings

--- wordhunt.py
+++ wordhunt.py
@@ -3,20 +3,28 @@ from boyer_moore import find_boyer_moore_all_paras
 from boyer_moore import find_any_patterns
 from analogy_strings import analogy_string_list
 from utils import para_to_pretty
+from utils import sent_to_pretty
 from personal import root as root
+from sentence_parser import *
+
+SOURCE_NAME = "BRWN"
+
+file_name = "analogy_sentences_test.txt"
+output_handler = open(root + "test_extractions/" + file_name, "w", encoding="utf-8")
+
+# csv_file = "names.csv"
+# csv_handler = open()
 
 brown_paras = brown.paras()
-
-#para_indices = find_boyer_moore_all_paras(brown_paras, ['is', 'like', 'a'])
 para_indices = find_any_patterns(brown_paras, analogy_string_list)
-print(para_indices)
-
-output_file_name = "test_001.txt"
-output_handler = open(root + "test_extractions/" + output_file_name, "w", encoding="utf-8")
-
 
 for para_index in para_indices:
-    print("--------", file = output_handler)
-    print(para_to_pretty(brown_paras[para_index]), file = output_handler)
+    sentence_pos = get_analogy_sentence(brown_paras[para_index], analogy_string_list)
+    sentence = sentence_pos[0]
+    sent_index = sentence_pos[1]
+    if sentence != '':
+        id_tag = "[" + SOURCE_NAME + ", PARA#" + str(para_index) + ", SENT#" + str(sent_index) + "]"
+        output_handler.write(id_tag)
+        output_handler.write(sentence + "\n")
 
 output_handler.close()
