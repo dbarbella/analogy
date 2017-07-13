9897daaee632042f09482cb4a147076cde1d420d
Vuong Khuat <vuongkhuat0407@gmail.com>
2017-07-13 15:26:12 -0400

    add the new sample and ground corpra from based on what we tagged.

--- analogy_classifier.py
+++ analogy_classifier.py
@@ -9,22 +9,22 @@ from boyer_moore import find_boyer_moore
 # Implementation of the classifier to detect analogy.
 # From http://www.nltk.org/book/ch06.html
 
-analogy_file_name = "\\test_extractions\\analogy_sentences.txt"
-non_analogy_file_name = "\\test_extractions\\demo_analogy_ground.txt"
+analogy_file_name = "\\test_extractions\\bc_samples.txt"
+non_analogy_file_name = "\\test_extractions\\bc_grounds.txt"
 
-def get_analogy_string2(text):
+def get_analogy_indicator(text):
     # Returns the first analogy string in the text if found one, empty string
     # otherwise.s
     for item in analogy_string_list:
         pattern = " ".join(item)
         if pattern != "as as":
-            if pattern in text: return {"analogy_string" : pattern}
+            if pattern in text: return {"analogy_indicator" : pattern}
         else:
             ptrn = "as .*\ as"
             match = re.search(ptrn, text)
-            if match != None: return {"analogy_string": pattern}
+            if match != None: return {"analogy_indicator": pattern}
 
-    return {"analogy_string" : ""}
+    return {"analogy_indicator" : ""}
 
 def get_analogy_string(text):
     # Returns a tuple of the first analogy indicator along with its speech tag in the text.
@@ -36,9 +36,9 @@ def get_analogy_string(text):
     for pattern in analogy_string_list:
         match_index = find_boyer_moore(tokens, pattern)
         if match_index != -1:
-            for i in range(len(pattern)):
-                result.append(tagged_text[match_index + i])
-            return {"analogy_indicator:" : tuple(result)}
+            # for i in range(len(pattern)):
+            #     result.append(tagged_text[match_index + i])
+            return {"analogy_indicator:" : tuple(pattern)}
 
     return {"analogy_indicator:" : tuple(result)}
 
@@ -58,8 +58,10 @@ def get_all_analogy_string(text):
     return {"analogy_indicator:" : tuple(result)}
 
 def get_pos_tags(text):
-    tokens = text.split()
-    return {"pos_tags:" : tuple(nltk.pos_tag(tokens))}
+    tokens = nltk.word_tokenize(text)
+    pos_tags = tuple([word[1] for word in nltk.pos_tag(tokens)])
+    pos_string = " ".join(pos_tags)
+    return {"pos_tags:" : pos_tags}
 
 def get_list(filename):
     # Returns all training data as a list
@@ -75,14 +77,14 @@ def get_list(filename):
 
 
 analogy_list = get_list(analogy_file_name)
-non_analogy_list = get_list(non_analogy_file_name)
+non_analogy_list = get_list(non_analogy_file_name)[: 150]
 
 # labeled data.
 samples = [(text, 'YES') for text in analogy_list] + [(text, 'NO') for text in non_analogy_list]
 random.shuffle(samples)
 
 # divide data into training set and test set
-feature_sets = [({"text:" : text}, label) for (text, label) in samples]
+feature_sets = [(get_pos_tags(text), label) for (text, label) in samples]
 mid_point = int(len(samples) / 2)
 train_set =  feature_sets[: mid_point]
 test_set = feature_sets[mid_point :]
@@ -92,14 +94,14 @@ classifier = nltk.NaiveBayesClassifier.train(train_set)
 
 # test classifier
 
-# Use the classifier to classify s1 and s2:
+#Use the classifier to classify s1 and s2:
 # s1 = "He talks like a person who knows everything."
 # s2 = "If the rain stops now, I will be able to go to work."
 # print(classifier.classify(get_analogy_string(s1)))
 # print(classifier.classify(get_analogy_string(s2)))
 
 print(nltk.classify.accuracy(classifier, test_set))
-print(classifier.show_most_informative_features(5))
+print(classifier.show_most_informative_features(10))
 
 # show the ones the classifier guessed wrong.
 # errors = []
--- analogy_strings.py
+++ analogy_strings.py
@@ -1,6 +1,6 @@
 analogy_string_list = [['is', 'like'], ['are', 'like'], ['like', 'a'], ['like', 'an'], ['analogous'],
 					['analogy'], ['similar', 'to'], ['similarly'], ['the', 'same', 'as'],
-					['likewise'], ['equally'], ['equivalent', 'to'], ['identical', 'to'],
+					['likewise'], ['equivalent', 'to'], ['identical', 'to'],
 					['comparable', 'to'], ['as', '', 'as']]
 
 #TODO: Update this list.
