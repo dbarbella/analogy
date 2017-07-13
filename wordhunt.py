38d96de5ea729981e0e5835167af414da3dd5e9d
Vuong Khuat <vuongkhuat0407@gmail.com>
2017-07-13 10:30:26 -0400

    re-add lost files

--- wordhunt.py
+++ wordhunt.py
@@ -1,4 +1,4 @@
-from nltk.corpus import brown
+from nltk.corpus import gutenberg
 from boyer_moore import find_boyer_moore_all_paras
 from boyer_moore import find_any_patterns
 from analogy_strings import analogy_string_list
@@ -8,24 +8,25 @@ from personal import root as root
 from sentence_parser import *
 import csv
 
-SOURCE_NAME = "BRWN"
+SOURCE_NAME = "GUT"
 
-file_name = "analogy_sentences.txt"
-output_handler = open(root + "test_extractions\\" + file_name, "w", encoding="utf-8")
+txt_file_name = "analogy_sentences_GUT.txt"
+csv_file_name = "analogy_names_GUT.csv"
+output_handler = open(root + "test_extractions\\" + txt_file_name, "w", encoding="utf-8")
 
 # Find the indices of all paragraphs that contain the patterns as listed in
 # analogy_string_list
-brown_paras = brown.paras()
-para_indices = find_any_patterns(brown_paras, analogy_string_list)
+paras = gutenberg.paras()
+para_indices = find_any_patterns(paras, analogy_string_list)
 ids = {}            # save sentences' ids in hash table to prevent duplicates.
 
 # Extract the exact sentences and write them to csv and txt files.
-with open(root + "test_extractions\\analogy_names.csv", 'w') as csvfile:
+with open(root + "test_extractions\\" + csv_file_name, 'w') as csvfile:
     fieldnames = ['name', 'text']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL, lineterminator='\n')
     writer.writeheader()
     for para_index in para_indices:
-        sentence_pos = get_analogy_sentence(brown_paras[para_index], analogy_string_list)
+        sentence_pos = get_analogy_sentence(paras[para_index], analogy_string_list)
         # get_analogy_sentence returns a 2-element tuple. The first element is the analogy string,
         # the second is its sentence index within the paragraph.
         sentence = sentence_pos[0]
