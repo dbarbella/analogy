4b8cdd7142ba9f26860b57b17128885d15a28cdb
Rei Rembeci <rei_rembeci@yahoo.com>
2017-07-18 09:51:58 -0400

    Added a few comments for each function

--- functions.py
+++ functions.py
@@ -33,75 +33,82 @@ def get_list(filename):
 
     return list
 
+# Returns all training data as a list which contains only the text(removing the source, paragraph #, sentence #, ratings
 def get_list_re(filename):
-    # Returns all training data as a list
-    # File should be formatted as a text line followed '>' in the next line
-    # before a new text line.
     list = []
     file = open(filename, "r", encoding = "utf-8")
     for line in file.readlines():
         if line[0] != '>' and line != "\n":
             l = line.split("]\",")[-1]
             final = re.sub("[^a-zA-Z]"," ", l)
-            #final = line.split("]\",")[-1].split(".")[0].split("?")[0].split("!")[0]
             list.append(final)
 
     return list
 
-# labeled data.
+# labeled data
 samples = [(text, 'YES') for text in analogy_list] + [(text, 'NO') for text in non_analogy_list]
 
+# preprocess the data so it can be used with SVMs and Neural Nets
 def preprocess_svm_neural(samples, percent_test):
     num_samples = len[samples]
     random.shuffle(samples)
     cutoff = int((1.0 - percent_test) * num_samples)
+    # create a train set and a test/development set
     feature_sets = [(text, label) for (text, label) in samples]
     train_set =  feature_sets[:cutoff]
     test_set = feature_sets[cutoff:]
+    # separate the training data and the training labels
     train_data = [text for (text, label) in train_set]
     train_labels = [label for (text, label) in train_set]
+    # separate the test data and the test labels
     test_data = [text for (text, label) in test_set]
     test_labels = [label for (text, label) in test_set]
     return(train_data, train_labels, test_data, test_labels)
 
+# preprocess the data so it can be used with Naive Bayes and Maximum Entropy(which are implemented through NLTK)
 def preprocess_naive_max(samples, percent_test):
     num_samples = len[samples]
     random.shuffle(samples)
     cutoff = int((1.0 - percent_test) * num_samples)
+    # create a train set and a test/development set
     feature_sets = [({"text:" : text}, label) for (text, label) in samples]
     train_set =  feature_sets[:cutoff]
     test_set = feature_sets[cutoff:]
     return(train_set, test_set)
     
+# Transform the data so it can be represented using tfidf    
 def tfidf(train_data, test_data):
     TfidfVect = TfidfVectorizer(tokenizer=lambda doc: doc, lowercase=False)
     TfidfTrans = TfidfVect.fit_transform(train_data)
     TfidfTrans_test = TfidfVect.transform(test_data)
     return(TfidfTrans, TfidfTrans_test)
 
+# Transform the data so it can be represented using Count Vectorizer
 def countvect(train_data, test_data):
     CountVect = CountVectorizer(lowercase=False)
     CountTrans = CountVect.fit_transform(train_data)
     CountTest = CountVect.transform(test_data)
     return(CountTrans, CountTest)
 
+# Transform the data so it can be represented using Hashing Vectorizer
 def hashing(train_data, test_data):
     HashVect = HashingVectorizer(lowercase=False)
     HashTrans = HashVect.fit_transform(train_data)
     HashTest = HashVect.transform(test_data)
     return(HashTrans, HashTest)
 
+# Implementetion of the fmeasure metric, which calculates the precision, recall and f1measure given a confusion matrix
 def fmeasure(matrix):
     precision = matrix[0][0] / (matrix[0][1] + matrix[0][0])
     recall = matrix[0][0] / (matrix[1][0] + matrix[0][0])
     f_measure = (2 * precision * recall) / (precision + recall)
     return(precision, recall, f_measure)
-    
-# within the if classifier = nn statement
-train_data, train_labels, test_data, test_labels = preprocess_svm_neural(samples)
+
+# A function which classifies data using different SVMs    
 def svm(train_data, train_labels, test_data, test_labels, representation, extra=[]):
     # if the classifier not specified, use SVC as the default one
     if extra == "":
+        # using tfidf representation
         if representation == "tfidf":
             TfidfTrans, TfidfTrans_test = tfidf(train_data, test_data)
             Svc_tf = SVC().fit(TfidfTrans, train_labels)
@@ -110,6 +117,7 @@ def svm(train_data, train_labels, test_data, test_labels, representation, extra=
             matrix = confusion_matrix(test_labels,test_predict_Svc_tf,labels=["YES", "NO"])
             precision, recall, f_measure = fmeasure(matrix)
             return(score, matrix, precision, recall, f_measure)
+        # using Count Vectorizer representation
         elif representation == "count":
             CountTrans, CountTest = countvect(train_data, test_data)
             Svc_count = SVC().fit(CountTrans, train_labels)
@@ -118,6 +126,7 @@ def svm(train_data, train_labels, test_data, test_labels, representation, extra=
             matrix = confusion_matrix(test_labels,test_predict_Svc_hash,labels=["YES", "NO"])
             precision, recall, f_measure = fmeasure(matrix)
             return(score, matrix, precision, recall, f_measure)
+        # using Hashing Vectorizer representation
         elif representation == "hash":
             HashTrans, HashTest = hashing(train_data, test_data)
             Svc_hash = SVC().fit(HashTrans, train_labels)
@@ -128,7 +137,7 @@ def svm(train_data, train_labels, test_data, test_labels, representation, extra=
             return(score, matrix, precision, recall, f_measure)
         else:
             sys.exit("This representation has not been implemented yet.")
-    # if the classifier is specified
+    # if the classifier is specified: linear/nusvc
     elif extra == "linear":
         if representation == "tfidf":
             TfidfTrans, TfidfTrans_test = tfidf(train_data, test_data)
@@ -186,8 +195,7 @@ def svm(train_data, train_labels, test_data, test_labels, representation, extra=
     else:
         sys.exit("This classifier has not been implemented yet.")
         
-# within the other if statement in main_interface        
-train_data, train_labels, test_data, test_labels = preprocess_svm_neural(samples)        
+# function which classifies data using MLP Neural Net classifier      
 def neural(train_data, train_labels, test_data, test_labels, representation):
     if representation = "tfidf":
         MLP_tf = MLPClassifier().fit(TfidfTrans, train_labels)
@@ -210,18 +218,17 @@ def neural(train_data, train_labels, test_data, test_labels, representation):
         matrix = confusion_matrix(test_labels,test_predict_MLP_hash,labels=["YES", "NO"]))
         precision, recall, f_measure = fmeasure(matrix)
         return(score, matrix, precision, recall, f_measure)
+    # if another representation is given as a parameter
     else:
         sys.exit("This classifier has not been implemented yet.")
 
-# within the other if statement 
-train_set, test_set = preprocess_naive_max(samples)
+# function which classifies data using the NLTK version of Naive Bayes
 def naive(train_set, test_set):
     classifier = nltk.NaiveBayesClassifier.train(train_set)
     score = nltk.classify.accuracy(classifier, test_set)
     return(score)
 
-# within the other if statement
-train_set, test_set = preprocess_naive_max(samples)
+# function which classifies data using the NLTK version of Maximum Entropy
 def max_ent(train_set, test_set):
     max_ent = nltk.classify.MaxentClassifier.train(train_set, 'GIS', trace=0, max_iter=1000)
     score = nltk.classify.accuracy(max_ent, test_set)
