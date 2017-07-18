5df87854fecc2051c5094d88cb6828c65e6cd4b3
dbarbella <barbeda@earlham.edu>
2017-07-18 09:36:50 -0400

    Merging main_interfacev1.py int main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -24,10 +24,10 @@ def analogy_trial(positive_set, negative_set, percent_test, representation, clas
         train_data, train_labels, test_data, test_labels = functions.preprocess_svm_neural(samples, percent_test)        
         functions.neural(train_data, train_labels, test_data, test_labels, representation)
     elif classifer == "naive":
-        train_set, test_set = functions.preprocess_naive_max(samples)
+        train_set, test_set = functions.preprocess_naive_max(samples, percent_test)
         functions.naive(train_set, test_set)
     elif clasifier = "max_ent":
-        train_set, test_set = functions.preprocess_naive_max(samples)
+        train_set, test_set = functions.preprocess_naive_max(samples, percent_test)
         functions.max_ent(train_set, test_set)
     else:
         sys.exit("This classifier has not been implemented yet.")
