3ca8862a6c7952ce53c2304b23058432c49d2449
Noah Back <nrback16@cs.earlham.edu>
2017-07-18 16:13:32 -0400

    fixed output destination to Logs/exp_trials

--- export_log.py
+++ export_log.py
@@ -12,13 +12,13 @@ def outputResults(testOutput):
     
     testOutput.insert(0, now)
      
-    if os.path.exists(OutputFileName+"_"+now+".csv"):
+    if os.path.exists("Logs/exp_trials/"+OutputFileName+"_"+now+".csv"):
         with open(OutputFileName+"_"+now+".csv", 'a') as resultsFile:
             fileReader = csv.reader(resultsFile)
             fileWriter = csv.writer(resultsFile, quoting=csv.QUOTE_ALL)
             fileWriter.writerow(testOutput)
     else:
-        with open(OutputFileName+"_"+now+".csv", 'w') as resultsFile:
+        with open("Logs/exp_trials/"+OutputFileName+"_"+now+".csv", 'w') as resultsFile:
             fileWriter = csv.writer(resultsFile, quoting=csv.QUOTE_ALL)
             fileWriter.writerow(["datetime", "positive_set", "negative_set", "representation", "classifier", "extra", "score", "matrix", "precision", "recall", "f_measure", "Runtime(seconds)", "Algorithm_time(seconds)", "Comments"])
             fileWriter.writerow(testOutput)
