f9626e01859522936ec0e5bfcf71d517b9e355b7
mashres15 <mashres15@tools.cs.earlham.edu>
2017-07-19 10:03:29 -0400

    Merge all csv trial files

---
+++ merge_csv.py
@@ -0,0 +1,20 @@
+import glob
+import csv
+
+path = 'Logs/exp_trials/*.csv'
+
+with open("Logs/consolidated/"+"consolidated_log.csv", 'w') as resultsFile:
+    fileWriter = csv.writer(resultsFile, quoting=csv.QUOTE_ALL)
+    fileWriter.writerow(["datetime", "positive_set", "negative_set", "representation", "classifier", "extra", "score", "matrix", "precision", "recall", "f_measure", "Runtime(seconds)", "Algorithm_time(seconds)", "Comments"])
+           
+    for fileName in glob.glob(path):
+        with open(fileName, 'r') as file:
+            fileReader = csv.reader(file)
+            next(fileReader, None)
+            for row in fileReader:
+                fileWriter.writerow(row)
+    print("File successfully consolidated at Logs/consolidated.")
+            
+        
+    
+    
