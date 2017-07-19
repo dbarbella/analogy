8770850c77fb74f75a0a0f090f57462b7c854875
Maniz Shrestha <mashres15@earlham.edu>
2017-07-19 15:58:14 -0400

    Update merge_csv.py

--- merge_csv.py
+++ merge_csv.py
@@ -1,12 +1,11 @@
 import glob
 import csv
 
-path = "/logs/exp_trials/"
-
+path = "logs/exp_trials/*.csv"
 with open("logs/consolidated/"+"consolidated_log.csv", 'w') as resultsFile:
     fileWriter = csv.writer(resultsFile, quoting=csv.QUOTE_ALL)
     fileWriter.writerow(["datetime", "positive_set", "negative_set", "representation", "classifier", "extra", "score", "matrix", "precision", "recall", "f_measure", "Runtime(seconds)", "Algorithm_time(seconds)", "Comments"])
-           
+
     for fileName in glob.glob(path):
         with open(fileName, 'r') as file:
             fileReader = csv.reader(file)
