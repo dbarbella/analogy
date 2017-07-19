c1433e95ad818cafc449f6409213026e15436fd9
Vuong Khuat <vuongkhuat0407@gmail.com>
2017-07-19 13:38:33 -0400

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -21,32 +21,19 @@ def analogy_trial(positive_set, negative_set, percent_test, representation, clas
     non_analogy_list = functions.get_list_re(negative_set)
     # Randomly divide them into a training set and a test set
     samples = [(text, 'YES') for text in analogy_list] + [(text, 'NO') for text in non_analogy_list]
+
     # Run classifier, generate results based on the value passed in for representation
     beginTimer = time.time()
-    if classifier == "svm":
-        train_data, train_labels, test_data, test_labels = functions.preprocess(samples, percent_test)
-        score, matrix, precision, recall, f_measure = (functions.svm(train_data, train_labels, test_data, test_labels, representation, extra))
-    elif classifier == "neural":
-        train_data, train_labels, test_data, test_labels = functions.preprocess(samples, percent_test)        
-        score, matrix, precision, recall, f_measure = (functions.neural(train_data, train_labels, test_data, test_labels, representation))
-    elif classifier == "naive":
-        train_data, train_labels, test_data, test_labels = functions.preprocess(samples, percent_test)
-        score, matrix, precision, recall, f_measure = (functions.naive(train_data, train_labels, test_data, test_labels, representation))
-    elif classifier == "max_ent":
-        train_data, train_labels, test_data, test_labels = functions.preprocess(samples, percent_test)
-        score, matrix, precision, recall, f_measure = (functions.max_ent(train_data, train_labels, test_data, test_labels, representation))
-    else:
-        sys.exit("This classifier has not been implemented yet.")
+    train_data, train_labels, test_data, test_labels = functions.preprocess(samples, percent_test)
+    score, matrix, precision, recall, f_measure = functions.classify(train_data, train_labels, test_data, test_labels, classifier, representation, extra)
+
     # Store results
     algoTime = time.time()-beginTimer
     runTime = time.time()-start
-    outputData = [positive_set, negative_set, representation, classifier, extra, score, matrix, precision, recall, f_measure, runTime, algoTime, comment] 
+    outputData = [positive_set, negative_set, representation, classifier, extra, score, matrix, precision, recall, f_measure, runTime, algoTime, comment]
     outputResults(outputData)
 
 if __name__ == '__main__':
     positive_set = 'test_extractions/bc_samples.txt'
     negative_set = 'test_extractions/bc_grounds.txt'
     analogy_trial(positive_set, negative_set, .5, 'tfidf', 'neural')
-    
-
-    
