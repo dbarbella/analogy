bd463f61916f1ed304f464f88d44101973118a52
nrback <nrback16@earlham.edu>
2017-10-09 16:18:29 -0400

    added "s" to representation

--- harness.py
+++ harness.py
@@ -3,7 +3,7 @@ from emails import send_email
 import traceback
 
 list_of_classifiers = ["naive", "svm", "max_ent", "neural"]
-list_of_representation = ["count", "tfidf", "hash"]
+list_of_representations = ["count", "tfidf", "hash"]
 list_of_extras = [{"sub_class":""}, {"sub_class":"linear", "stop_words":'english', "max_df":0.8, "activation":"tanh", "learning_rate":"adaptive"}]
 positive_set = 'test_extractions/bc_samples.txt'
 negative_set = 'test_extractions/bc_grounds.txt'
