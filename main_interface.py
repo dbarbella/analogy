10dd19655e6c502f6a886a69672e11ee5dcff640
Rei Rembeci <rrembe15@earlham.edu>
2017-10-23 15:17:41 -0400

    Added the temporary analogy_pipeline function

--- main_interface.py
+++ main_interface.py
@@ -59,9 +59,29 @@ def analogy_trial(positive_set, negative_set, percent_test, representation, clas
     outputData[1] = outputData[1].tolist()
     return outputData
 
+def analogy_pipeline(positive_set, negative_set, percent_test, representation, classifier, extra={"sub_class":""}, timer=1000000000, comment=""):
+
+    start = time.time()
+    # Read in the set of positive examples
+    analogy_list = functions.get_list_re(positive_set)
+    # Read in the set of negative examples
+    non_analogy_list = functions.get_list_re(negative_set)
+    # Randomly divide them into a training set and a test set
+    samples = [(text, 'YES') for text in analogy_list] + [(text, 'NO') for text in non_analogy_list]
+    extra = functions.set_extra(extra)
+    # Run classifier, generate results based on the value passed in for representation
+    beginTimer = time.time()
+    train_data, train_labels, test_data, test_labels = functions.preprocess(samples, percent_test)
+    # Make sure the classifier runs within a set time
+    score, matrix, precision, recall, f_measure = functions.classify_pipeline(train_data, train_labels, test_data, test_labels, classifier, representation, extra, timer)
+    print(score)
+    print(matrix)
+    print(precision, recall, f_measure)
+       
+
 if __name__ == '__main__':
     positive_set = 'test_extractions/bc_samples.txt'
-    negative_set = 'test_extractions/bc_grounds.txt'
-    #analogy_trial(positive_set, negative_set, .5, 'count', 'neural', {"sub_class":"nusvc", "stop_words":'english', "max_df":0.8, "activation":"tanh", "learning_rate":"adaptive"})
-    analogy_trial(positive_set, negative_set, .5, "count", "naive", {"sub_class":""}, timer= 5)
+    negative_set = 'test_extractions/random_grounds.txt'
+    analogy_pipeline(positive_set, negative_set, .5, 'tfidf', 'svm')
+
     
