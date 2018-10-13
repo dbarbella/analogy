b381bd08b671bb699f491d711f33b4ee7cf8ff1c
hungphi98 <nguyenhungphi2698@Gmail.com>
2018-10-13 17:04:45 -0400

    replacing analogy signals

--- 2018/c_command.py
+++ 2018/c_command.py
@@ -1,13 +1,18 @@
 import os
 import nltk
 import csv
+from time import time
 from nltk.parse import stanford
+from nltk import Tree
+from nltk.draw.util import CanvasFrame
+from nltk.draw import TreeWidget
 os.chdir('..')
 jar = './stanford-parser/jars/stanford-parser.jar'
 model = './stanford-parser/jars/stanford-english-corenlp-2018-02-27-models.jar'
 parser = stanford.StanfordParser(model,jar,encoding = 'utf8')
 
-# this file  is used to draw tree and print tree images.
+
+
 class Node:
     def __init__(self,value):
         self.value = value
@@ -39,48 +44,85 @@ class Node:
                     return child
             return self.parent.target_search()
 
-def readFile(fileName):
-    sent =[]
-    with open(fileName) as file:
-        readcsv = csv.reader(file, delimiter=',')
-        for row in readcsv:
-            sentence = row[1]
-            tag = row[0]
-            # num_tag.append(tag)
-            sent.append(sentence)
-    return sent
+class Extract:
+    def __init__(self):
+        self.parsing_time = 0
+        self.base_time = 0
+        self.target_time = 0
+        self.createTree_time = 0
 
-def search(sent,w,l):
-    base_val, target_val = None,None
-    parsed_sent = parser.raw_parse(sent)
-    for line in parsed_sent:
-        base,target = None,None
-        root = Node(line[0])
-        root.createTree()
-        base = root.base_search(w,l)
-        if base is not None:
-            target = base.target_search()
-            base_val = base.value
-        if target is not None:
-            target_val = target.value
-    return base_val,target_val
+    def readFile(self,fileName):
+        sent =[]
+        with open(fileName) as file:
+            readcsv = csv.reader(file, delimiter=',')
+            for row in readcsv:
+                sentence = row[1]
+                sent.append(sentence)
+        return sent
 
+    def search(self,sent,w,l):
+        base_val, target_val, base, target = None,None,None,None
+        start1 = time()
+        parsed_sent = parser.raw_parse(sent)
+        start2 = time()
+        self.parsing_time += start2 - start1
+        for line in parsed_sent:
+            root = Node(line[0]) #create the root node
+            root.createTree() #create a tree based on the parsed tree
+            start3 = time()
+            self.createTree_time += start3 - start2
+            base = root.base_search(w,l) #search for base
+            end = time()
+            self.base_time += end - start3
+            if base is not None: #if base is found then search for target
+                target = base.target_search() #tranverse to the target from the base
+                end2 = time()
+                self.target_time += end2 - end
+                base_val = base.value #for returning
+            if target is not None:
+                target_val = target.value #for returning
+        return base_val,target_val
 
-if __name__ == '__main__':
-    sentences = readFile('./corpora/verified_analogies.csv')
-    count,like_count = 0,0
+    def read_by_line(self,fileName):
+        signals = []
+        with open(fileName) as file:
+            signals = file.readlines()
+        signals = [x.strip() for x in signals]
+        return signals
 
+if __name__ == "__main__":
+    extract = Extract()
+    signals = extract.read_by_line("./2018/analogy_signals.txt")
+    sentences = extract.readFile('./corpora/verified_analogies.csv')
+    count,like_count,i = 0,0,0
+    base_found = 0
+    base, target = None,None
     for s in sentences:
-        base, target = search(s,"like","PP")
+        if "like" in s:
+            base, target = extract.search(s,"like","PP")
+        else:
+            for signal in signals:
+                if signal in s:
+                    s = s.replace(signal, "like")
+                    base, target = extract.search(s,"like", "PP")
+                    break
         if base is not None and target is not None:
             count+=1
         else:
-            print(s)
-        words = s.split()
-        if "like" in words:
-            like_count+=1
-
-    print("Number of found base and target: ",count)
-    print("Number of the word 'like' in a sentence: ", like_count)
-
-    # GUI
+            parsed_sent = parser.raw_parse(s)
+            for line in parsed_sent:
+                cf = CanvasFrame()
+                t = Tree.fromstring(str(line))
+                tc = TreeWidget(cf.canvas(), t)
+                cf.add_widget(tc, 10, 10)
+                i += 1
+                cf.print_to_file('./2018/undetectedCases/tree' + str(i) + '.ps')
+                tree_name = './2018/undetectedCases/tree' + str(i) + '.ps'
+                tree_new_name = './2018/undetectedCases/tree' + str(i) + '.png'
+                os.system('convert ' + tree_name + ' ' + tree_new_name)
+                cf.destroy()
+    print("parsing time: ", extract.parsing_time)
+    print("base time: ", extract.base_time)
+    print("target time: ", extract.target_time)
+    print("create tree time: ", extract.createTree_time)
+    print("found cases ", count)
--- 2018/parser.py
+++ 2018/parser.py
@@ -36,141 +36,142 @@ sentences = []
 lower_tie = 4
 upper_tie = 157
 
+base_time, tartget_time, createTree_time, parsing_time = 0, 0, 0, 0
 
+class Grammar_parse:
+    def __init__(self):
+        self.parsing_time = 0
+        self.base_time = 0
+        self.target_time = 0
+        self.createTree_time = 0
+    def linking_word_check(self,sent):
+        # print(sent)
+        linking_word = ["like","assss","similar","equivalent","similarly","equivalently","anagolous"]
+        txt = nltk.word_tokenize(sent)
+        list_of_word = nltk.pos_tag(txt)
+        ind = 0
+        for w in linking_word:
+            if w in txt:
+                ind = txt.index(w)
+        if list_of_word[ind][0].lower() == linking_word[0].lower() or list_of_word[ind][0] == linking_word[1]:
+            if list_of_word[ind][1] == 'IN':
+                base,target = self.parse(sent,list_of_word[ind][0],"IN")
+                return base, target
 
-def linking_word_check(sent):
-    # print(sent)
-    linking_word = ["like","assss","similar","equivalent","similarly","equivalently","anagolous"]
-    txt = nltk.word_tokenize(sent)
-    list_of_word = nltk.pos_tag(txt)
-    ind = 0
-    for w in linking_word:
-        if w in txt:
-            ind = txt.index(w)
-    if list_of_word[ind][0].lower() == linking_word[0].lower() or list_of_word[ind][0] == linking_word[1]:
-        if list_of_word[ind][1] == 'IN':
-            base,target = parse(sent,list_of_word[ind][0],"IN")
-            print(base,  target)
-            return base, target
+            if list_of_word[ind][1] == 'JJ':
+                base,target = self.parse(sent, list_of_word[ind][0], "JJ")
+                return base, target
+            else:
+                return None,None
 
-        if list_of_word[ind][1] == 'JJ':
-            base,target = parse(sent, list_of_word[ind][0], "JJ")
-            print(base,  target)
+        elif list_of_word[ind][0] == linking_word[4] or list_of_word[ind][0] == linking_word[5]:
+            base,target = self.parse(sent, list_of_word[ind][0], "RB")
             return base, target
         else:
             return None,None
 
-    elif list_of_word[ind][0] == linking_word[4] or list_of_word[ind][0] == linking_word[5]:
-        base,target = parse(sent, list_of_word[ind][0], "RB")
-        print(base, target)
-        return base, target
-    else:
-        return None,None
+    def recursive_search(self,p,w,l,phrase, node):
+        for i in range(len(p)):
+            if type(p[i]) == nltk.Tree:
+                if p[i]._label == l:
+                    if p[i][0] == w:
+                        if len(p) > i + 1:
+                            return p[i+1],p
+                elif p[i]._label in phrases:
+                    for j in p[i]:
+                        phrase,node = self.recursive_search(j,w,l,phrase,node)
+                if len(p[i]) > 1:
+                    phrase,node = self.recursive_search(p[i],w,l,phrase, node)
+        return phrase,node
 
-def recursive_search(p,w,l,phrase, node):
-    for i in range(len(p)):
-        if type(p[i]) == nltk.Tree:
-            if p[i]._label == l:
-                if p[i][0] == w:
-                    if len(p) > i + 1:
-                        return p[i+1],p
-            elif p[i]._label in phrases:
-                for j in p[i]:
-                    phrase,node = recursive_search(j,w,l,phrase,node)
-            if len(p[i]) > 1:
-                phrase,node = recursive_search(p[i],w,l,phrase, node)
-    return phrase,node
+    def parse(self,sent,w,l):
+        phrase = None
+        base = None
+        target = None
+        start1 = time()
+        parsed_sent = parser.raw_parse(sent)
+        start2 = time()
+        self.parsing_time += start2 - start1
+        for line in parsed_sent:
+            parent_node = Node(line)
+            target, node =  self.recursive_search(line[0],w,l,phrase, parent_node)
+            start3 = time()
+            self.base_time += start3 - start2
+            parent_node = self.creatingParentNode(line, parent_node)
+            start4 = time()
+            self.createTree_time = start4 - start3
+            base = self.base_search2(target,parent_node,base)
+            start5 = time()
+            self.target_time += start5 - start4
+        return base,target
 
-def parse(sent,w,l):
-    phrase = None
-    base = None
-    target = None
-    parsed_sent = parser.raw_parse(sent)
-    for line in parsed_sent:
-        parent_node = Node(line)
-        target, node =  recursive_search(line[0],w,l,phrase, parent_node)
-        parent_node = creatingParentNode(line, parent_node)
-        base = base_search2(target,parent_node,base)
-    return base,target
+    def base_search2(self,target,parent_node,base):
+        for child in parent_node.children:
+            if str(child.value) == str(target):
+                temp = child.parent
+                p_child = {}
+                result = []
 
-def base_search2(target,parent_node,base):
-    for child in parent_node.children:
-        if str(child.value) == str(target):
-            temp = child.parent
-            p_child = {}
-            result = []
+                while temp is not None and temp.value._label not in noun :
+                    p_child[temp.parent] = temp
+                    temp = temp.parent
 
-            while temp is not None and temp.value._label not in noun :
-                p_child[temp.parent] = temp
-                temp = temp.parent
+                for temp in p_child:
+                    i = 0
+                    childNode = p_child[temp]
+                    while temp is not None and (temp.value[i]) != (childNode.value) and i < len(temp.value) - 1 :
+                        if temp.value[i]._label not in pronoun:
+                            result.append(temp.value[i])
+                            # print("temp\n",temp.value)
+                        i+=1
+                subject = None
+                if len(result) > 0:
+                    firstVerb = None
+                    subject = result.pop()
+                    if type(subject[0]) == nltk.tree.Tree:
+                        # print(subject[0]._label)
+                        if len(result)>0:
+                            if result[len(result)-1]._label in verb:
+                                firstVerb = result.pop()
+                            if subject[0]._label == 'PRP':
+                                f = []
+                                f.append(subject)
+                                f.append(firstVerb)
+                                f += result
+                                return f
+                            else:
+                                return subject
+                return subject
+            base = self.base_search2(target,child,base)
+        return base
 
-            for temp in p_child:
-                i = 0
-                childNode = p_child[temp]
-                while temp is not None and (temp.value[i]) != (childNode.value) and i < len(temp.value) - 1 :
-                    if temp.value[i]._label not in pronoun:
-                        result.append(temp.value[i])
-                        # print("temp\n",temp.value)
-                    i+=1
-            subject = None
-            if len(result) > 0:
-                firstVerb = None
-                subject = result.pop()
-                if type(subject[0]) == nltk.tree.Tree:
-                    # print(subject[0]._label)
-                    if len(result)>0:
-                        if result[len(result)-1]._label in verb:
-                            firstVerb = result.pop()
-                        if subject[0]._label == 'PRP':
-                            f = []
-                            f.append(subject)
-                            f.append(firstVerb)
-                            f += result
-                            return f
-                        else:
-                            return subject
-            return subject
-        base = base_search2(target,child,base)
-    return base
+    def creatingParentNode(self,key, node):
+        for count in range(len(key)):
+            if type(key[count]) == nltk.tree.Tree:
+                child = Node(key[count])
+                child.parent = node
+                subtree = self.creatingParentNode(key[count],child)
+                node.children.append(subtree)
+        return node
 
+    def checkAvailabilityNode(self,key):
+        count = len(key)
+        for i in range(count):
+            if len(key[i]) > 1:
+                return True
+        return False
+    def segmentWords(self,s):
+        return s.split()
 
-def segmentWords(s):
-    return s.split()
-num_tag = []
 def readFile(fileName):
     sent =[]
     with open(fileName) as file:
         readcsv = csv.reader(file, delimiter=',')
         for row in readcsv:
             sentence = row[1]
-            tag = row[0]
-            num_tag.append(tag)
             sent.append(sentence)
     return sent
 
-
-def creatingParentNode(key, node):
-    for count in range(len(key)):
-        if type(key[count]) == nltk.tree.Tree:
-            child = Node(key[count])
-            child.parent = node
-            subtree = creatingParentNode(key[count],child)
-            node.children.append(subtree)
-    return node
-
-def checkAvailabilityNode(key):
-    count = len(key)
-    for i in range(count):
-        if len(key[i]) > 1:
-            return True
-    return False
-
-
-
-# print(len(sentences))
-# p_sent = []
-# start = time()
-# c = lower_tie
 base = []
 target = []
 trash = [",","'", '"', "`"]
@@ -188,22 +189,31 @@ def writeCSVFile(text_output, to_dir):
     f.close()
 
 if __name__ == '__main__':
-    pos = readFile('./corpora/verified_analogies.csv')
-    neg = readFile('./corpora/verified_non_analogies.csv')
-    samples = pos + neg
-    # samples = readFile('./corpora/dmb_open_test.csv')
+    # pos = readFile('./corpora/verified_analogies.csv')
+    # neg = readFile('./corpora/verified_non_analogies.csv')
+    # samples = pos + neg
+    samples = readFile('./corpora/verified_analogies.csv')
     random.seed(1234)
     random.shuffle(samples)
     txt = ""
-    for i in range(300):
-        if samples[i] is not None:
-            base,target = linking_word_check(samples[i])
-            print(str(base) + "___"+ str(target))
-            if base is not None and target is not None:
-                txt += '"' + samples[i] + '","' + str(1) + '"\n'
-            else:
-                txt += '"' + samples[i] + '","' + str(0) + '"\n'
-    writeCSVFile(txt, './random_parse_tree.csv')
+
+    count = 0
+    grammar_parse = Grammar_parse()
+    for s in samples:
+        base,target, = grammar_parse.linking_word_check(s)
+        if base and target is not None:
+            count +=1
+            # print(str(base) + "___"+ str(target))
+            # if base is not None and target is not None:
+            #     txt += '"' + samples[i] + '","' + str(1) + '"\n'
+            # else:
+            #     txt += '"' + samples[i] + '","' + str(0) + '"\n'
+    print("parsing time: ", grammar_parse.parsing_time)
+    print("base time: ", grammar_parse.base_time)
+    print("target time: ", grammar_parse.target_time)
+    print("create tree time: ", grammar_parse.createTree_time)
+
+    # writeCSVFile(txt, './random_parse_tree.csv')
 # for i in range(lower_tie,upper_tie):
 #     p_sent.append(tryoutParent(sentences[i]))
 # for p in p_sent:
