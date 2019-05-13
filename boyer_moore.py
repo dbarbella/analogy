bedf4aa464eeeb54c2f2429ba4a3d9a3bfe370d5
dbarbella <barbeda@earlham.edu>
2019-05-13 14:27:08 -0400

    Deal with special characters.

--- boyer_moore.py
+++ boyer_moore.py
@@ -12,7 +12,39 @@ def find_boyer_moore(T, P):
     while i < n:
         if T[i] == P[k]:            # if chars are equal
             if k == 0:
-                return i     # check if Patter is complete
+                return i     # check if Pattern is complete
+            else:
+                i -= 1                  # normal iteration
+                k -= 1
+        else:
+            # if j < k (remember k index at P)
+            # shift i += m - (j+1)
+            # if j > k
+            # shift i += m - k
+            j = last.get(T[i], -1)     # -1 if item not there
+            i += m - (min(k, j+1))
+            k = m - 1
+    return -1
+
+def test_lists():
+    print("Testing...")
+    x = find_boyer_moore_lists(['in', 'the', 'stifling', 'heat', 'were', 'two', 'english', 'girls', 'in', 'traditional', 'blue', 'and', 'white', 'ghanaian', 'dress', 'playing', 'literary', 'bingo', 'and', 'demonstrating', 'solar', 'eclipses', 'with', 'a', 'football', 'and', 'an', 'orange.walking', 'back', 'with', 'laura', 'after', 'taking', 'pito', 'with', 'the', 'other', 'masters,', 'i', 'asked', 'her', 'what', 'it', 'was', 'like', 'living', 'and', 'working', 'in', 'ghana.'],['was', 'like'])
+    print(x)
+
+def find_boyer_moore_lists(T, P):
+    """ return lowest index of T at which the sublist P begins or -1"""
+    #print(T,P)
+    n, m = len(T), len(P)
+    if m == 0: return 0
+    last = {}               # Using hash table for fast access
+    for k in range(m):
+        last[P[k]] = k
+    i = m - 1           # i index at T, k index at P
+    k = m - 1                    # j index of last occurrence of T[i] in P
+    while i < n:
+        if T[i] == P[k]:            # if chars are equal
+            if k == 0:
+                return i     # check if Pattern is complete
             else:
                 i -= 1                  # normal iteration
                 k -= 1
@@ -84,3 +116,7 @@ def find_any_patterns(paras_list, pattern_list):
         #print(next_indices)
         para_indices = para_indices + next_indices
     return para_indices
+
+
+if __name__ == "__main__":
+    test_lists()
