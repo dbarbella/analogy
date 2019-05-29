e0f90036c2fa471016c6a78a50c47f75c4a6f0eb
Davit Kvarskhava <dkvart17@earlham.edu>
2019-05-29 11:30:29 -0400

    updated the documentation and the instructions for glove_functions

--- glove_functions.py
+++ glove_functions.py
@@ -1,4 +1,14 @@
 #By Davit Kvartskhava
+''' This code includes a number of functions
+	that can be used to explore the glove corpus and the multidimensional vector space associated with it.
+	The main feature is the analogy finder (fill_the_gap method), which given three words, 
+	A, B and C outputs some D. C to D is like A to B.'''
+
+''' In order to run the fill_the_gap method with your set of words, just change the list_of_analogies global variable'''
+
+'''The first thing that has to be run is parse_co_occurence_matrix, which parses the csv file 
+	(one of the glove pre-trained vectors that can be downloaded here: https://github.com/stanfordnlp/GloVe) 
+	and stores the vectors into a dictionary (<word, vector>) '''
 import numpy
 
 DICTIONARY = {}
@@ -31,13 +41,26 @@ def print_item(X):
 def magnitude(vector):
 	return numpy.linalg.norm(vector)
 
-#assuming we are given numpy arrays.
+#assuming we are given two words in the string format,
+#it returns the cosine value between the vectors associated
+#with those words.
 def cosine_for_words(word1, word2):
 	vect1 = DICTIONARY[word1.lower()]
 	vect2 = DICTIONARY[word2.lower()]
 	return cosine(vect1, vect2)
 
+#this is for vectors.
+def cosine(vect1, vect2):
 
+	magnitude_product = magnitude(vect1) * magnitude(vect2)
+	if magnitude_product == 0: #we want to avoid division by 0.
+		return 1
+	else:
+		return numpy.dot(vect1, vect2) / magnitude_product 
+
+#given a base vector, returns a sorted array containing all the words
+#that are sorted based on cosine similarity.
+#The first element in the list will be contextually closest to the base vector.
 def sort_by_cos_for_vector(base_vect):
 	return_list = [] #list of tuples -> (cos_value, word)
 	for next_word in DICTIONARY:
@@ -48,6 +71,8 @@ def sort_by_cos_for_vector(base_vect):
 	return_list.sort(key=lambda tup: tup[0], reverse = True)
 	return return_list
 
+#Same as the upper function, only difference is that the argument passed
+#is a word instead of a vector. (don't judge me)
 #returns an array sorted in a descending order by cos_value
 def sort_by_cos_similarity(base_word):
 	return_list = [] #list of tuples -> (cos_value, word)
@@ -60,6 +85,7 @@ def sort_by_cos_similarity(base_word):
 	return return_list
 
 ################################
+#Given four words, prints the vectors AB and CD
 def compare(A, B, C, D):
 
 	first = DICTIONARY[A] - DICTIONARY[B]
@@ -67,7 +93,6 @@ def compare(A, B, C, D):
 	print("subtract ", A, " from ", B, "\n Result: ",first)
 	print("subtract ", C, " from ", D, "\n Result: ",second)
 
-#CAN IMPLEMENT THIS IN A BETTER WAY!!! __ DID IT
 #works for descending order only
 def find_closest_range(lst, pivot):
 	result_list = []
@@ -107,23 +132,31 @@ def find_closest_range(lst, pivot):
 
 	return result_list
 
-#USING cosine_for_words SIMILARITY ________ DOESN"T WORK VERY WELL
+#Finding the cosine value between A and B. After that, 
+#goes through the corpus and sorts every word by cosine value
+#relative to the base_word. After that, finds the words that have 
+#cosines with respect to base_word closer to that cosine between A and B.
+#NOTE!!! ---- DOESN'T HAVE A GOOD PERFORMANCE
 def fill_the_gap_cos(A, B, base_word):
-	original_cos = cosine_for_word(A, B)
+	original_cos = cosine_for_word(A, B) 
 
 	sorted_list = sort_by_cos_similarity(base_word)
-	probable_ans_llist = find_closest_range(sorted_list, original_cos)####TO DOOO#######
-	print(original_cos)
+	probable_ans_llist = find_closest_range(sorted_list, original_cos)
 	
 	for next_word in probable_ans_llist:
 		print(next_word)
 
-#find D based on vector given by B - A + C
+#find D =  B - A + C, and then
+#find the words close to D based on cosine similarity. 
 def fill_the_gap_euc(A, B, base_word):
 	answer_vec = DICTIONARY[B] - DICTIONARY[A] + DICTIONARY[base_word]
 	sorted_to_answer = sort_by_cos_for_vector(answer_vec)
 	return sorted_to_answer[:RANGE]
 
+#Merges both euclidean and cosine similarity to produce a 
+#list of words that could be the possible correct answers. 
+#In addition to that, we are not calculating cosine similarity
+#from the origin, but from C.(makes a big difference)
 def sort_by_cos_relative_to(direction_vect, relative_to_vect):#direction_vect ->> D, #relative_to_vect ->> C
 	return_list_cos = [] #list of tuples -> (cos_value, word)
 	return_list_dis = []
@@ -145,15 +178,6 @@ def sort_by_cos_relative_to(direction_vect, relative_to_vect):#direction_vect ->
 			if next_cos_tuple[1] == next_dist_tuple[1]:
 				return_list.append((next_cos_tuple[0], next_dist_tuple[1], next_dist_tuple[0]))
 	return return_list
-
-#this is for vectors.
-def cosine(vect1, vect2):
-
-	magnitude_product = magnitude(vect1) * magnitude(vect2)
-	if magnitude_product == 0:
-		return 1
-	else:
-		return numpy.dot(vect1, vect2) / magnitude_product
 	
 
 #relative_to_vect = C
@@ -163,6 +187,9 @@ def relative_cosine(vect1, vect2, relative_to_vect):
 
 	return cosine(new_vect1, new_vect2)
 
+#################################
+'''Prints RANGE number of words A to B is like C to ____?'''
+#################################
 def fill_the_gap(A, B, base_word):
 	relative_to_vect = DICTIONARY[base_word] #vector C
 	direction_vect = DICTIONARY[B] - DICTIONARY[A] + relative_to_vect #Vector D
@@ -171,20 +198,11 @@ def fill_the_gap(A, B, base_word):
 	for i in range(RANGE):
 		print(sorted_to_answer[i])
 
-# print(relative_cosine(numpy.array([1,2,3]), numpy.array([4,5,6]), numpy.array([1,2,3])))
-#print(magnitude(numpy.array([1,2,3,4,5])))
+
 parse_co_occurence_matrix("../glove.6B/glove.6B.100d.txt")
-#print("FIRST:", cosine_for_words("king", "queen"), "SECOND:", cosine_for_words("queen","king"))
-#fill_the_gap("man", "police", "wife")
-list_of_analogies = [("walk", "walked", "go"),("queen", "king", "man"),("boy", "son", "girl"),("kitten", "cat", "puppy"),("france", "paris", "germany"), ("bird", "feather", "dog"), ("steam", "gas", "ice"), ("day", "night", "light"), ("dark", "darker", "windy"), ("like", "love", "dislike")]
+
+list_of_analogies = [("walk", "walked", "go"),("queen", "king", "woman"),("boy", "son", "girl"),("kitten", "cat", "puppy"),("france", "paris", "germany"), ("bird", "feather", "dog"), ("steam", "gas", "ice"), ("day", "night", "light"), ("dark", "darker", "windy"), ("like", "love", "dislike")]
 
 for i in list_of_analogies:
 	fill_the_gap(i[0], i[1], i[2])
 	print("\n")
-
-# lst = sort_by_cos_similarity("Georgia")
-# for i in range(20):
-# 	print(lst[i])
-# compare("king", "queen", "man", "woman")
-# print(cosine_for_words("king","queen"))
-# print(cosine_for_words("man", "woman"))
