0da8e22024e3ddaa6c7693f208ae4c007ebb2a11
dbarbella <david.barbella@gmail.com>
2019-08-09 22:52:30 -0400

    keras_rnn.py now contains a more complete test harness.

--- nn/keras/keras_rnn.py
+++ nn/keras/keras_rnn.py
@@ -30,6 +30,8 @@ default_analogies_file = '../../corpora/verified_analogies.csv'
 default_glove_directory = '../../corpora/glove/'
 default_glove_file = 'glove.6B.100d.txt'
 default_results_file = './results/results-variability-test.txt'
+default_spreadsheet_file = './results/results-sheet.csv'
+default_experiments_file = './results/experiments.txt'
 
 num_folds = 4
 num_epochs = 10
@@ -63,111 +65,160 @@ if num_args < 4:
 else:
     glove_file = sys.argv[3]
 
-# Start the timer
-start_time = time.time()
-start_datetime = datetime.now()
-
-analogy_sentences = readCSV(analogies_file, 1)
-num_analogy_sentences = len(analogy_sentences)
-non_analogy_sentences = readCSV(non_analogies_file, 1)
-num_non_analogy_sentences = len(non_analogy_sentences)
-
-# Create a numpy array of the correct labels.
-labels = np.asarray([1] * num_analogy_sentences + [0] * num_non_analogy_sentences)
-
-# Create a list of all of the sentences
-all_sentences = analogy_sentences + non_analogy_sentences
-
-# Tokenize the sentences using the keras tokenizer
-# Why are we limiting the number of words to 10000?
-tokenizer = Tokenizer(num_words=lexicon_size)
-tokenizer.fit_on_texts(all_sentences)
-sequences = tokenizer.texts_to_sequences(all_sentences)
-
-word_index = tokenizer.word_index
-print("Found {} unique tokens".format(len(word_index)))
-
-# pad sequences to a fixed length
-padded_sequences = pad_sequences(sequences, maxlen=max_sen_length_in_words)
-
-# An np array that contains a range of values from 0 to (n-1)
-indices = np.arange(padded_sequences.shape[0])
-
-# This is the first place it becomes nondeterministic
-np.random.shuffle(indices)  # shuffle np array that contains the indices
-
-# Arrange the padded sequences and their labels in the same random order.
-padded_sequences = padded_sequences[indices]
-labels = labels[indices]
-
-# Split into training and testing sets.
-train_data = padded_sequences[:len(padded_sequences) - num_test_samples]
-test_data = padded_sequences[len(padded_sequences) - num_test_samples:]
-train_labels = labels[:len(padded_sequences) - num_test_samples]
-test_labels = labels[len(padded_sequences) - num_test_samples:]
-num_val_samples = len(train_data) // num_folds
-
-embedding_index = produce_embedding_index(glove_file)
-embedding_matrix = produce_embedding_matrix(lexicon_size, embedding_dim, embedding_index, word_index)
-
-# k-fold training - Each fold chooses a new slice of the training data to be the validation set
-# This should probably be its own function - likely a mutator that takes the model as an argument?
-model, val_acc_histories, acc_histories = train_model(num_val_samples, train_data, train_labels, lexicon_size,
-                                                      embedding_dim, max_sen_length_in_words, embedding_matrix,
-                                                      num_epochs, epoch_batch_size, num_folds)
-
-test_loss, test_accuracy, results_confusion_matrix = evaluate_model(model, test_data, test_labels)
-
-# Stop the timer
-stop_time = time.time()
-elapsed_time = stop_time - start_time
-
 
 # This will need to record all of the parameters used, along with the results.
-def record_results(results_file=default_results_file):
+def record_results(trial_results_dict, results_file=default_results_file, results_sheet=default_spreadsheet_file):
     print("Recording results...")
     results_handler = open(results_file, 'a')
     results_handler.write("="*30 + "\n")
-    results_handler.write("Start time: " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
+    sheet_handler = open(results_sheet, 'a')
+
+    start_datetime = trial_results_dict["start_datetime"]
+    results_handler.write("Start time: " + str(start_datetime.strftime('%Y-%m-%d %H:%M:%S')) + "\n")
+    sheet_handler.write(str(start_datetime.strftime('%Y-%m-%d %H:%M:%S'))+",")
+
+    elapsed_time = trial_results_dict["elapsed_time"]
     results_handler.write("Elapsed time (s): " + str(elapsed_time) + "\n")
+    sheet_handler.write(str(elapsed_time) + ",")
+
+    num_analogy_sentences = trial_results_dict["num_analogy_sentences"]
+    num_non_analogy_sentences = trial_results_dict["num_non_analogy_sentences"]
+
 
     # Record information about the training data
-    results_handler.write("Analogies file: " + analogies_file + "\n")
-    results_handler.write("Non-Analogies file: " + non_analogies_file + "\n")
+    results_handler.write("Analogies file: " + analogies_file + "\n")  # This is a global for now.
+    results_handler.write("Non-Analogies file: " + non_analogies_file + "\n")  # This is a global for now.
     results_handler.write("Analogies: " + str(num_analogy_sentences) +
                           " Non-Analogies: " + str(num_non_analogy_sentences) +
                           " Total: " + str(num_analogy_sentences + num_non_analogy_sentences) + "\n")
+    sheet_handler.write(analogies_file+","+non_analogies_file+","+str(num_analogy_sentences)+","+
+                        str(num_non_analogy_sentences)+","+str(num_analogy_sentences + num_non_analogy_sentences)+",")
 
     # Record information about the NN setup
-    results_handler.write("Embedding vector dimensions: " + str(embedding_dim) + "\n")
+    results_handler.write("Embedding vector dimensions: " + str(embedding_dim) + "\n")  # This is a global for now.
+    sheet_handler.write(str(embedding_dim)+",")
 
     # Record information about the training.
-    results_handler.write("Training epochs: " + str(num_epochs) + "\n")
-    results_handler.write("Epoch batch size: " + str(epoch_batch_size) + "\n")
-    results_handler.write("Training folds: " + str(num_folds) + "\n")
+    results_handler.write("Training epochs: " + str(num_epochs) + "\n")  # This is a global for now.
+    results_handler.write("Epoch batch size: " + str(epoch_batch_size) + "\n")  # This is a global for now.
+    results_handler.write("Training folds: " + str(num_folds) + "\n")  # This is a global for now.
+    sheet_handler.write(str(num_epochs)+","+str(epoch_batch_size)+","+str(num_folds)+",")
 
     # Record information about the results.
+
+    test_loss = trial_results_dict["test_loss"]
+    test_accuracy = trial_results_dict["test_accuracy"]
+    results_confusion_matrix = trial_results_dict["results_confusion_matrix"]
+
     results_handler.write("Testing Loss: " + str(test_loss) + "\n")
     results_handler.write("Testing Accuracy: " + str(test_accuracy) + "\n")
     results_handler.write("Confusion Matrix:\n " + str(results_confusion_matrix) + "\n")
+    sheet_handler.write(str(test_loss) + "," + str(test_accuracy) + ","+"\n")
 
     # Record the full dehydrated model - Consider whether we want this.
     # results_handler.write("Model configuration:" + str(model.get_config()) + "\n")
     # Record a summary of the model
+    model = trial_results_dict["model"]
     model.summary(print_fn=lambda x: results_handler.write(x + '\n'))
 
     results_handler.write("\n")
+
     results_handler.close()
+    sheet_handler.close()
 
 
-record_results()
+# This should almost certainly produce a dictionary.
+def run_trial():
+    #global num_analogy_sentences, num_non_analogy_sentences, model, val_acc_histories, acc_histories, test_loss, test_accuracy, results_confusion_matrix, elapsed_time
+    trial_results_dict = {}
+    # Start the timer
+    start_time = time.time()
+    start_datetime = datetime.now()
+    trial_results_dict["start_time"] = start_time
+    trial_results_dict["start_datetime"] = start_datetime
+
+    analogy_sentences = readCSV(analogies_file, 1)
+    num_analogy_sentences = len(analogy_sentences)
+    non_analogy_sentences = readCSV(non_analogies_file, 1)
+    num_non_analogy_sentences = len(non_analogy_sentences)
+
+    trial_results_dict["analogy_sentences"] = analogy_sentences
+    trial_results_dict["num_analogy_sentences"] = num_analogy_sentences
+    trial_results_dict["non_analogy_sentences"] = non_analogy_sentences
+    trial_results_dict["num_non_analogy_sentences"] = num_non_analogy_sentences
+
+    # Create a numpy array of the correct labels.
+    labels = np.asarray([1] * num_analogy_sentences + [0] * num_non_analogy_sentences)
+    # Create a list of all of the sentences
+    all_sentences = analogy_sentences + non_analogy_sentences
+    # Tokenize the sentences using the keras tokenizer
+    # Why are we limiting the number of words to 10000?
+    tokenizer = Tokenizer(num_words=lexicon_size)
+    tokenizer.fit_on_texts(all_sentences)
+    sequences = tokenizer.texts_to_sequences(all_sentences)
+    word_index = tokenizer.word_index
+    print("Found {} unique tokens".format(len(word_index)))
+    # pad sequences to a fixed length
+    padded_sequences = pad_sequences(sequences, maxlen=max_sen_length_in_words)
+    # An np array that contains a range of values from 0 to (n-1)
+    indices = np.arange(padded_sequences.shape[0])
+    # This is the first place it becomes nondeterministic
+    np.random.shuffle(indices)  # shuffle np array that contains the indices
+    # Arrange the padded sequences and their labels in the same random order.
+    padded_sequences = padded_sequences[indices]
+    labels = labels[indices]
+    # Split into training and testing sets.
+    train_data = padded_sequences[:len(padded_sequences) - num_test_samples]
+    test_data = padded_sequences[len(padded_sequences) - num_test_samples:]
+    train_labels = labels[:len(padded_sequences) - num_test_samples]
+    test_labels = labels[len(padded_sequences) - num_test_samples:]
+    num_val_samples = len(train_data) // num_folds
+    embedding_index = produce_embedding_index(glove_file)
+    embedding_matrix = produce_embedding_matrix(lexicon_size, embedding_dim, embedding_index, word_index)
+    # k-fold training - Each fold chooses a new slice of the training data to be the validation set
+    # This should probably be its own function - likely a mutator that takes the model as an argument?
+    model, val_acc_histories, acc_histories = train_model(num_val_samples, train_data, train_labels, lexicon_size,
+                                                          embedding_dim, max_sen_length_in_words, embedding_matrix,
+                                                          num_epochs, epoch_batch_size, num_folds)
+
+    trial_results_dict["model"] = model
+    trial_results_dict["val_acc_histories"] = val_acc_histories
+    trial_results_dict["acc_histories"] = acc_histories
+
+    test_loss, test_accuracy, results_confusion_matrix = evaluate_model(model, test_data, test_labels)
+
+    trial_results_dict["test_loss"] = test_loss
+    trial_results_dict["test_accuracy"] = test_accuracy
+    trial_results_dict["results_confusion_matrix"] = results_confusion_matrix
+
+    # Stop the timer
+    stop_time = time.time()
+    elapsed_time = stop_time - start_time
+    trial_results_dict["elapsed_time"] = elapsed_time
+
+    record_results(trial_results_dict)
+    return trial_results_dict
 
 
-def plot():
+accuracies = []
+for i in range(20):
+    trial_results_dict = run_trial()
+    accuracies.append(trial_results_dict["test_accuracy"])
+
+print(accuracies)
+print(sum(accuracies)/len(accuracies))
+
+
+
+
+
+def plot(trial_results_dict):
     """
     Plots the results.
     :return: No return value.
     """
+    val_acc_histories = trial_results_dict["val_acc_histories"]
+    acc_histories = trial_results_dict["acc_histories"]
     avg_acc_history = [np.mean([x[epoch] for x in acc_histories]) for epoch in range(num_epochs)]
     avg_val_acc_history = [np.mean([x[epoch] for x in val_acc_histories]) for epoch in range(num_epochs)]
     import matplotlib.pyplot as plt
